7ca0dd27b1f96f4b436da6d3c1aa2cf631f8044d
aasujape-cmyk <aasujape@gmail.com>
2026-05-26 12:37:16 +0200

    product filter and user,section .json added

--- Scripts/readProducts.gd
+++ Scripts/readProducts.gd
@@ -2,18 +2,29 @@ extends Node
 
 var products=[]
 var productsFiltered=[]
+var sections=[]
 
-func ready():
+func _ready():
 	products=readProducts()
+	sections=readSections()
+	#productsSection("Vegetables")
 
 func readProducts():
-	var file = FileAccess.open("res://Assets/products.json", FileAccess.READ)
+	var file = FileAccess.open("res://Assets/json/products.json", FileAccess.READ)
 	var content = file.get_as_text()
 	var parseContent=JSON.parse_string(content)
 	return parseContent
-
+	
+func readSections():
+	var file = FileAccess.open("res://Assets/json/sections.json", FileAccess.READ)
+	var content = file.get_as_text()
+	var parseContent=JSON.parse_string(content)
+	return parseContent
+	
 func productsSection(section):
+	productsFiltered.clear()
 	if section!=null:
 		for pro in products:
 			if(pro.section==section):
-				productsFiltered.push(pro)
+				productsFiltered.push_back(pro)
+				#print(productsFiltered)
