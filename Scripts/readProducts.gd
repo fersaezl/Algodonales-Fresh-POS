02f3c3f8d801714f9698e6b4ed886259eac9226e
aasujape-cmyk <aasujape@gmail.com>
2026-05-27 12:23:54 +0200

    search filter

--- Scripts/readProducts.gd
+++ Scripts/readProducts.gd
@@ -7,6 +7,8 @@ var sections=[]
 func _ready():
 	products=readProducts()
 	sections=readSections()
+	searchProducts("ESTREllA GALICIA")
+	print(productsFiltered)
 	#productsSection("Vegetables")
 
 func readProducts():
@@ -28,3 +30,11 @@ func productsSection(section):
 			if(pro.section==section):
 				productsFiltered.push_back(pro)
 				#print(productsFiltered)
+				
+func searchProducts(string):
+	productsFiltered.clear()
+	for pro in products:
+		if(string.to_lower() in pro.productName.to_lower()):
+			productsFiltered.push_back(pro)
+	if(productsFiltered.size()==0):
+		print("No hay productos con eses filtro")
