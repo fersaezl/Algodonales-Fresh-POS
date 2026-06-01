a2b42e63babf76f6d7bfaa0c6d6531bb5454777c
aasujape-cmyk <aasujape@gmail.com>
2026-06-01 09:21:39 +0200

    search filter

--- Scripts/mainpos.gd
+++ Scripts/mainpos.gd
@@ -13,7 +13,7 @@ var first_btn
 
 func _ready() -> void:
 	load_categories()
-	var first_btn = categories_grid.get_child(0)
+	first_btn = categories_grid.get_child(0)
 	load_products("all", first_btn)
 
 func load_categories() -> void:
@@ -75,5 +75,11 @@ func addCart(product, quantity):
 	cartTSCN.add_product(product, cart)
 
 func _on_search_bar_text_changed(new_text: String) -> void:
+	if (new_text==""):
+		load_products("all", first_btn)
+		return
+	if current_btn!=first_btn:
+		current_btn.set_selected(false)
+		first_btn.set_selected(true)
 	ProductManager.searchProducts(new_text)
 	show_products(ProductManager.productsFiltered)
